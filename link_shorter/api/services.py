f1f6152bfa7f268ae74cdfb67b4a453c3ccec618
Денис Мурашов <murashov@zelrun.com>
2023-01-29 14:40:39 +0600

    add tests for redirection and url_creation

--- link_shorter/api/services.py
+++ link_shorter/api/services.py
@@ -1,6 +1,7 @@
-from .models import Tokens
 from django.http import HttpResponse
 from django.shortcuts import redirect
+
+from .models import Tokens
 
 
 def get_full_url(url: str) -> str:
@@ -8,15 +9,15 @@ def get_full_url(url: str) -> str:
     Достаем полную ссылку по short_url
     Если ссылки нет в базе или она не активна
     возвращаем ошибку.
-    Если все ок, то добавляем к счеткику перезодов 1
+    Если все ок, то добавляем к счетчику статистики 1
     и возвращаем полную ссылку.
     """
     try:
         token = Tokens.objects.get(short_url__exact=url)
         if not token.is_active:
-            raise KeyError("Ссылка больше не доступна")
+            raise KeyError('Token is no longer available')
     except Tokens.DoesNotExist:
-        raise KeyError("В базе нет такой ссылки")
+        raise KeyError('Try another url. No such urls in DB')
     token.requests_count += 1
     token.save()
     return token.full_url
--- link_shorter/api/tests.py
+++ link_shorter/api/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Tokens
+
+
+class TestAPI(APITestCase):
+    """Тестируем POST запросы"""
+    url = '/api/tokens/'
+
+    def test_token_creation(self):
+        """Проверка создания токенра через api"""
+        data = {
+            'full_url': 'http://post.url.test.ru'
+        }
+        response = self.client.post(self.url, data=data)
+        result = response.json()
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(result['full_url'], 'http://post.url.test.ru')
+        self.assertEqual(len(result['short_url']), 6)
+        self.assertIsInstance(result, dict)
+        self.assertEqual(Tokens.objects.all().count(), 1)
+
+
+class TestRedirection(TestCase):
+    """Тестируем GET запросы"""
+    active_url = '/aEdj01'
+    deactive_url = '/q2Nb23'
+
+    def setUp(self) -> None:
+        Tokens.objects.create(
+            full_url='https://ya.ru/',
+            short_url='aEdj01',
+        )
+
+        Tokens.objects.create(
+            full_url='https://stackoverflow.com/',
+            short_url='q2Nb23',
+            is_active=False
+        )
+
+    def test_redirection(self):
+        """Тестируем счетчик запросов токена"""
+        response = self.client.get(self.active_url)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, 'https://ya.ru/')
+
+    def test_response_counter(self):
+        """Тестируем счетчик запросов токена"""
+        self.assertEqual(
+            Tokens.objects.get(short_url='aEdj01').requests_count, 0
+        )
+        self.client.get(self.active_url)
+        self.assertEqual(
+            Tokens.objects.get(short_url='aEdj01').requests_count, 1
+        )
+        self.assertEqual(
+            Tokens.objects.get(short_url='q2Nb23').requests_count, 0
+        )
+        self.client.get(self.deactive_url)
+        self.assertEqual(
+            Tokens.objects.get(short_url='q2Nb23').requests_count, 0
+        )
+
+    def test_deactive_url(self):
+        """Тестируем неактивные токены"""
+        response = self.client.get(self.deactive_url)
+        self.assertEqual(response.content, b'Token is no longer available')
